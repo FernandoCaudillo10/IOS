6b0962cbef9ae0dd3ada44b68c3c7ecc770fab01
Guillermo Che <ch3tt3rq@Guillermos-MacBook-Pro.local>
2019-09-23 18:56:01 -0700

    Tumbler

---
+++ Tumbler/Tumbler/PhotoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoTableViewCell.swift
+//  Tumbler
+//
+//  Created by Guillermo Che on 9/18/19.
+//  Copyright © 2019 caudillo. All rights reserved.
+//
+
+import UIKit
+
+class PhotoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Tumbler/Tumbler/PhotosViewController.swift
@@ -0,0 +1,67 @@
+//
+//  PhotosViewController.swift
+//  Tumbler
+//
+//  Created by Guillermo Che on 9/18/19.
+//  Copyright © 2019 caudillo. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoTableViewCell") as! PhotoTableViewCell
+        
+        // Configure YourCustomCell using the outlets that you've defined.
+        
+        return cell
+    }
+    
+    var posts: [[String: Any]] = []
+    @IBOutlet weak var photosTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        photosTableView.delegate = self
+        photosTableView.dataSource = self
+        
+        let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        session.configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data,
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                print(dataDictionary)
+                
+                // Get the dictionary from the response key
+                let responseDictionary = dataDictionary["response"] as! [String: Any]
+                // Store the returned array of dictionaries in our posts property
+                self.posts = responseDictionary["posts"] as! [[String: Any]]
+                
+                // TODO: Reload the table view
+            }
+        }
+        task.resume()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
